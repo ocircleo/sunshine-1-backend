eec6d90ce5f46aa9028246608088533aab91029b
vacoule <salmanhossain11222626@hotmail.com>
2023-07-24 21:22:06 +0600

    server finished

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 1111;
 const cors = require("cors");
@@ -32,6 +32,12 @@ async function run() {
 
     app.get("/jobs", async (req, res) => {
       const result = await jobs.find().toArray();
+      res.send(result);
+    });
+    app.get("/detaill/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await companys.findOne(query);
       res.send(result);
     });
     app.get("/topcompanys/:isbd/:num", async (req, res) => {
